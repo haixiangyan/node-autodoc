60f198651cca29bef4cf50d2bdbe2ffa56f64614
Haixiang <haixiang6123@gmail.com>
2021-05-02 00:02:56 +0800

    feat: add title and description

--- test/AutoDocAgent.test.js
+++ test/AutoDocAgent.test.js
@@ -1,41 +1,72 @@
 import express from 'express'
 import AutoDocAgent from '../lib/index'
 
+const app = express();
+const agent = new AutoDocAgent(app);
+
+app.get('/', function (req, res) {
+  res.send({ msg: 'get success', code: 0 });
+});
+
+app.post('/', function (req, res) {
+  res.json({ msg: 'post success', code: 0 })
+})
+
 describe('AutoDocAgent', function () {
   it('should get correct docMeta from get request', function (done) {
-    const app = express();
-    const agent = new AutoDocAgent(app);
-
-    app.get('/', function (req, res) {
-      res.send({ hostname: req.hostname });
-    });
-
     agent
-      .host('something.test')
-      .get('/?a=1&b=2')
-      .send('hello')
+      .get('/?a=1&b=2', { title: 'Get all users', description: 'Send a get request to get all users from the server' })
       .end(function (err, res) {
         if (err) return done(err);
-        // supertest assertion
-        expect(res.body.hostname).toEqual('something.test')
-
         // Get docMeta
         const docMeta = agent.getDocMeta('get', '/?a=1&b=2')
         expect(docMeta).not.toBeNull()
 
         const {request, response} = docMeta
 
+        // title and description
+        expect(docMeta.title).toEqual('Get all users')
+        expect(docMeta.description).toEqual('Send a get request to get all users from the server')
+
         // Request assertion
         expect(request.method).toEqual('get')
         expect(request.url).toEqual('/?a=1&b=2')
-        expect(request.body).toEqual('hello')
         expect(request.params).toEqual({a: '1', b: '2'});
 
         // Response assertion
-        expect(response.body).toEqual({hostname: 'something.test'})
+        expect(response.body).toEqual({ msg: 'get success', code: 0 })
 
-        agent.render()
         done();
       })
   });
+
+  it('should get correct docMeta from post request', function (done) {
+    agent
+      .post('/?a=1&b=2', { title: 'Post a user', description: 'Create a user and add it to the database' })
+      .send({ name: 'Jack', password: '123' })
+      .end(function (err, res) {
+        if (err) return done(err);
+        // Get docMeta
+        const docMeta = agent.getDocMeta('post', '/?a=1&b=2')
+        expect(docMeta).not.toBeNull()
+
+        const {request, response} = docMeta
+
+        // title and description
+        expect(docMeta.title).toEqual('Post a user')
+        expect(docMeta.description).toEqual('Create a user and add it to the database')
+
+        // Request assertion
+        expect(request.method).toEqual('post')
+        expect(request.url).toEqual('/?a=1&b=2')
+        expect(request.body).toEqual({ name: 'Jack', password: '123' })
+        expect(request.params).toEqual({a: '1', b: '2'});
+
+        // Response assertion
+        expect(response.body).toEqual({ msg: 'post success', code: 0 })
+        done();
+      })
+  });
+
+  afterAll(() => agent.render())
 });
