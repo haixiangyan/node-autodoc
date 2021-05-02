4f0de7095d6270e30b32dad9c14bb6135dad188f
Haixiang <haixiang6123@gmail.com>
2021-05-02 20:20:42 +0800

    style: add more info for home page

--- test/teardown.js
+++ test/teardown.js
@@ -5,6 +5,9 @@ const agents = [booksAgent, usersAgent]
 
 module.exports = async () => {
   AutoDocAgent.renderIndex({
+    title: 'My API Documentation',
+    description: 'This is my first documentation for testing, haha~',
+    author: 'Haixiang',
     agents,
     outputDir,
     templateDir
