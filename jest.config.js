aa4161c25579cc72b7595a978e5ffcf67a52f66d
Haixiang <haixiang6123@gmail.com>
2021-05-03 13:21:21 +0800

    feat: remove unnecessary codes

--- jest.config.js
+++ jest.config.js
@@ -151,6 +151,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     '/node_modules/',
+    '/example/',
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
